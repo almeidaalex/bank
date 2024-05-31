04b4d7f8959e12e1d345c8be490ee7f7c273bdf6
Alex Almeida <alex.almeida@bouvet.no>
2024-05-31 10:15:04 +0200

    core: add multiple runtimes to the projects#

--- Bank.Api/Program.cs
+++ Bank.Api/Program.cs
@@ -19,9 +19,6 @@ namespace Bank.Api
 
     public static IHostBuilder CreateHostBuilder(string[] args) =>
         Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-              webBuilder.UseStartup<Startup>();
-            });
+            .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>());
   }
 }
