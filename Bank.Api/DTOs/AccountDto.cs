942bf032fc4a6cafcc26e89304d46333ddbcc6df
Alex Almeida <alex.almeida@bouvet.no>
2024-10-09 22:32:15 +0200

    refact: convert DTO classes to record

--- Bank.Api/DTOs/AccountDto.cs
+++ Bank.Api/DTOs/AccountDto.cs
@@ -1,33 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 using Bank.Domain;
-using Bank.Domain.Contracts;
 
-namespace Bank.Api.DTOs
+namespace Bank.Api.DTOs;
+
+public record AccountDto(int AccountNo, string Title, decimal Balance, IEnumerable<AccountStatementDto> Statements)
 {
-  public class AccountDto
+  public static implicit operator AccountDto(Account entity)
   {
-    public AccountDto(int accountNo, string title, decimal balance, IEnumerable<AccountStatementDto> statements)
-    {
-      AccountNo = accountNo;
-      Title = title;
-      Statements = statements;
-      Balance = balance;
-    }
-
-    public int AccountNo { get; }
-    public string Title { get; }
-    public decimal Balance { get; }
-
-    public IEnumerable<AccountStatementDto> Statements { get; }
-
-    public static implicit operator AccountDto(Account entity)
-    {
-      var stats = entity.Operations.Select(h => (AccountStatementDto)h);
-      return new(entity.No, entity.Owner?.Title, entity.Balance, stats);
-    }
+    var stats = entity.Operations.Select(operation => (AccountStatementDto)operation);
+    return new(entity.No, entity.Owner?.Title, entity.Balance, stats);
   }
 }
--- Bank.Api/DTOs/InvoiceDto.cs
+++ Bank.Api/DTOs/InvoiceDto.cs
@@ -1,30 +1,22 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 using Bank.Domain;
 
-namespace Bank.Api.DTOs
+namespace Bank.Api.DTOs;
+
+
+
+public record InvoiceDto(
+  int Number,
+  decimal Amount,
+  DateTime DueDate
+)
 {
-  public class InvoiceDto
+
+  public static implicit operator Invoice(InvoiceDto dto)
   {
-    const string REQUIRED_MSG = "O campo {0} é obrigatório";
-
-    [Display(Name = "Número")]
-    [Required(ErrorMessage = REQUIRED_MSG)]
-    public int? Number { get; set; }
-
-    [Display(Name = "Valor")]
-    [Required(ErrorMessage = REQUIRED_MSG)]
-    public decimal? Amount { get; set; }
-
-    [Display(Name = "Data de Vencimento")]
-    [DataType(DataType.Date)]
-    [Required(ErrorMessage = REQUIRED_MSG)]
-    public DateTime? DueDate { get; set; }
-
-    public static implicit operator Invoice(InvoiceDto dto)
-    {
-      return new Invoice(dto.Number.GetValueOrDefault(), dto.DueDate.GetValueOrDefault(), dto.Amount.GetValueOrDefault());
-    }
+    return new Invoice(dto.Number, dto.DueDate, dto.Amount);
   }
 }
