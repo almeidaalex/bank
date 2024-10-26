45e1a0e006b1bfea603d22f1088c23e1547f428d
Alex Almeida <alex.almeida@bouvet.no>
2024-10-26 23:28:23 +0200

    feat(Iac): add container app, user identity and acr

---
+++ Bank.IaC/Program.cs
@@ -0,0 +1,91 @@
+﻿using Pulumi;
+using Pulumi.AzureNative.App;
+using Pulumi.AzureNative.App.Inputs;
+using Pulumi.AzureNative.ContainerRegistry;
+using ContainerRegistryInputs = Pulumi.AzureNative.ContainerRegistry.Inputs;
+using Pulumi.AzureNative.ManagedIdentity;
+using Pulumi.AzureNative.Resources;
+using ManagedServiceIdentityType = Pulumi.AzureNative.App.ManagedServiceIdentityType;
+
+
+
+return await Pulumi.Deployment.RunAsync(() =>
+{
+  string region = "norwayeast";
+  string shortRegion = "noe";
+  string appName = "bankapp";
+
+
+  // Create an Azure Resource Group
+  var resourceGroup = new ResourceGroup($"rg-{shortRegion}-{appName}", new ResourceGroupArgs
+  {
+    ResourceGroupName = $"rg-{shortRegion}-{appName}",
+    Location = region
+  });
+
+  var appUserIdentity = new UserAssignedIdentity($"uai-{shortRegion}-{appName}", new UserAssignedIdentityArgs
+  {
+    ResourceGroupName = resourceGroup.Name,
+    Location = region
+  });
+
+  var acr = new Registry($"acr{shortRegion}{appName}", new RegistryArgs
+  {
+    ResourceGroupName = resourceGroup.Name,
+    Location = resourceGroup.Location,
+    Sku = new ContainerRegistryInputs.SkuArgs { Name = "Basic" },
+  });
+
+  var containerEnv = new ManagedEnvironment($"cae-{shortRegion}-{appName}", new ManagedEnvironmentArgs
+  {
+    ResourceGroupName = resourceGroup.Name,
+    Location = resourceGroup.Location,
+  });
+
+  var containerApp = new ContainerApp($"ca-{shortRegion}-{appName}", new ContainerAppArgs
+  {
+    ResourceGroupName = resourceGroup.Name,
+    Location = resourceGroup.Location,
+    EnvironmentId = containerEnv.Id,
+    Configuration = new ConfigurationArgs
+    {
+      Registries = [
+        new RegistryCredentialsArgs {
+          Server = acr.LoginServer,
+          Identity = appUserIdentity.PrincipalId,
+        }
+      ],
+    },
+    Identity = new ManagedServiceIdentityArgs
+    {
+      Type = ManagedServiceIdentityType.UserAssigned,
+      UserAssignedIdentities = { { appUserIdentity.Id } }
+    },
+    Template = new TemplateArgs
+    {
+      Containers = {
+         new ContainerArgs
+         {
+           Name = "bankapp",
+           Image = $"{acr.LoginServer}/bankapp:latest",
+           Probes = {
+             new ContainerAppProbeArgs
+             {
+                Type = Type.Readiness,
+                HttpGet = new ContainerAppProbeHttpGetArgs
+                {
+                  Path = "/health",
+                  Port = 80
+                },
+                InitialDelaySeconds = 5,
+                PeriodSeconds = 10,
+                FailureThreshold = 3
+             },
+           }
+         }
+       }
+    }
+  });
+
+
+});
